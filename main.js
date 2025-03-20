7ac0ee5357431e7da1c68e2c0c9a2f028d438879
Sai Vijay <saivijay.rayalavarapu@gmail.com>
2025-03-20 23:06:18 +0530

    feat: main.js is added

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-const btn = document.querySelector('.btn');
-const urlInput = document.getElementById('uri');
-const inputContainer = document.querySelector('.input');
+var btnGet = document.querySelector("#btnGet")
+var data = document.querySelector("#data")
+var qrcode = document.querySelector("#qrcode")
 
-btn.addEventListener('click', () => {
-    const urlValue = urlInput.value.trim();
-
-    if(!urlValue){
-        return alert("Enter TEXT or URL");
+btnGet.addEventListener("click", function () {
+    if (data.value == "") {
+        alert('Name or URL is required !!')
+    } else {
+        getQRCode(data.value)
     }
-
-    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(urlValue)}`;
-    // https://api.qrserver.com/v1/create-qr-code/ which is the endpoint provided by the QRServer API to generate QR codes.
-
-    let qrImg = document.getElementById('qrcode');
-
-    if (!qrImg) {
-        qrImg = document.createElement('img');
-        qrImg.setAttribute('id', 'qrcode');
-        inputContainer.appendChild(qrImg);
-    }
-    qrImg.style.width = '150px';
-
-    qrImg.src = qrCodeUrl;
-});
+})
+async function getQRCode(data) {
+    var apiURL = "https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=" + data
+    var imgSrc = await fetch(apiURL)
+    console.log(imgSrc)
+    qrcode.src = imgSrc.url
+}
