6b57394a67b769344527a914712caa2ad15f7960
Sai Vijay <saivijay.rayalavarapu@gmail.com>
2025-03-21 18:55:03 +0530

    feat: main.js is added

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ btnGet.addEventListener("click", function () {
     }
 })
 async function getQRCode(data) {
-    var apiURL = "https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=" + data
-    var imgSrc = await fetch(apiURL)
-    console.log(imgSrc)
-    qrcode.src = imgSrc.url
+    var apiURL = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&margin=20&data=${encodeURIComponent(data)}`;
+    var imgSrc = await fetch(apiURL);
+    console.log(imgSrc);
+    qrcode.src = imgSrc.url;
 }
